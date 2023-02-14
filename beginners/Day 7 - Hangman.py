9d682e6f1bd7aca50abe994d7e2c338a649a18d0
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2023-02-14 16:54:09 +0000

    Update Day 7 - Hangman.py

--- beginners/Day 7 - Hangman.py
+++ beginners/Day 7 - Hangman.py
@@ -1,4 +1,4 @@
-#Step 1 
+import os
 import random
 
 stages = ['''
@@ -68,50 +68,60 @@ word_list = ['aardvark', 'balloon', 'cactus', 'daffodil', 'elephant', 'frosty', 
 'lawyer', 'musician', 'nurse', 'optometrist', 'plumber', 'quilter', 'receptionist', 'surgeon', 'teacher', 'university', 'veterinarian', 
 'writer', 'x-ray', 'yoga', 'zoologist']
 
+play_again = True
+while play_again:
+    chosen_word = random.choice(word_list)
+    display = []
+    for letter in chosen_word:
+            display.append("_")
 
-chosen_word = random.choice(word_list)
-display = []
-for letter in chosen_word:
-        display.append("_")
+    lives = 6
+    game = True
+    os.system('clear')
+    while game:
+        
+        if lives == 6:
+            print (stages[6])
+        elif lives == 5:
+            print (stages[5])
+        elif lives == 4:
+            print (stages[4])
+        elif lives == 3:
+            print (stages[3])
+        elif lives == 2:
+            print (stages[2])
+        elif lives == 1:
+            print (stages[1])
+        elif lives == 0:
+            print (stages[0])
 
-lives = 6
-game = True
+        print (' '.join(display))
 
-while game:
-    guess = input("guess a letter: ").lower()   
-    
-    if guess in chosen_word:
-        print("Right")
-        for i in range(len(chosen_word)):
-            if guess == chosen_word[i]:
-                display[i] = guess         
-    else:
-        print("Wrong")
+        guess = input("\nguess a letter: ").lower()   
+        
+        if guess in chosen_word:
+            print("Right")
+            for i in range(len(chosen_word)):
+                if guess == chosen_word[i]:
+                    display[i] = guess         
+        else:
+            print("Wrong")
 
-    print (' '.join(display))
+       
 
-    if "_" in display:
-        game = True
-    else: 
-        game = False
-
-    if guess not in chosen_word:
-        lives -= 1
-        if lives <=0:
-            print ("You lose")
+        if "_" in display:
+            game = True
+        else: 
             game = False
 
-    if lives == 6:
-        print (stages[6])
-    elif lives == 5:
-        print (stages[5])
-    elif lives == 4:
-        print (stages[4])
-    elif lives == 3:
-        print (stages[3])
-    elif lives == 2:
-        print (stages[2])
-    elif lives == 1:
-        print (stages[1])
-    elif lives == 0:
-        print (stages[0])
+        if guess not in chosen_word:
+            lives -= 1
+            if lives <=0:
+                print ("You lose")
+                game = False
+        os.system('clear')
+
+
+    again = input("would you like to play again? y/n: ")
+    if again != "y":
+        play_again = False
